02e70b516fe9a171a36f5da359dd7d1d1ee6aadc
mnshmangukiya <mnshmangukiya@gmail.com>
2023-10-06 16:53:52 +0530

    change package json main and app

--- public/js/main.js
+++ public/js/main.js
@@ -18,7 +18,7 @@ const getInfo = async(event) =>{
 
     }else{
         try{
-            let url = `http://api.openweathermap.org/data/2.5/weather?q=${cityVal}&units=metric&appid=8581f3e22a3ead1233063d00cfc7a2d6`;
+            let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityVal}&units=metric&appid=8581f3e22a3ead1233063d00cfc7a2d6`;
             const response = await fetch(url);
             const data = await response.json();
            const arrData = [data];
